f7468a3fa2e1248e2c073a73d148a0c4483518f1
Guilherme Mendes <guilherme737@gmail.com>
2017-02-14 08:43:06 -0200

    beta 0.1

--- app/index.js
+++ app/index.js
@@ -6,19 +6,19 @@ _ = require('lodash');
     //require('bootstrap-datepicker');
     //require('../node_modules/bootstrap-datepicker/dist/locales/bootstrap-datepicker.pt-BR.min');
 
-moment = require('moment');
-require('../node_modules/moment/locale/pt-br');
+//moment = require('moment');
+//require('../node_modules/moment/locale/pt-br');
 require('angular');
-require('angular-animate');
+//require('angular-animate');
 require('angular-resource');
 require('angular-ui-router');
-require('angular-ui-bootstrap');
+//require('angular-ui-bootstrap');
 require('angular-route');
 require('angular-local-storage');
 //require('../node_modules/ng-sanitize/index');
 require('angular-sanitize');
-require('selectize');
-require('select2');
+//require('selectize');
+//require('select2');
 require('../node_modules/ui-select/dist/select');
 
 require('ngbootbox');
@@ -27,7 +27,7 @@ require('ngbootbox');
 require('angular-simple-logger');
 require('angular-google-maps');
 
-alertify = require('alertifyjs');
+//alertify = require('alertifyjs');
 
 // angular
 require('./auth/auth.module');
